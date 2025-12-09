13207ef230f62bac0caa16e490c00c44ef2e170e
gorsestarrr <andrey.podymov@outlook.com>
2025-12-09 21:42:16 +0100

    day 09 **

--- 2025/day_09.cpp
+++ 2025/day_09.cpp
@@ -1,31 +1,63 @@
 #include <bits/stdc++.h>
+#include <boost/geometry.hpp>
+#include <boost/geometry/geometries/point_xy.hpp>
+#include <boost/geometry/geometries/polygon.hpp>
+#include <boost/geometry/geometries/box.hpp>
 
 using ll = long long;
 using ull = unsigned long long;
 using namespace std;
 
-struct Tile {
-    ll x, y;
-};
+namespace bg = boost::geometry;
+using Point = bg::model::d2::point_xy<ll>;
+using Polygon = bg::model::polygon<Point>;
+using Box = bg::model::box<Point>;
 
-ll area(const Tile& first, const Tile& second) {
-    return (abs(first.x - second.x ) + 1) * ( abs(first.y - second.y) + 1 );
+ll area(const Point& first, const Point& second) {
+    return (abs(first.x() - second.x() ) + 1) * ( abs(first.y() - second.y()) + 1 );
 }
 
-ll part1(const vector<Tile>& tiles) {
+ll part1(const vector<Point>& tiles) {
     int n = tiles.size();
     ll maxArea = -1;
-    ll at = 0;
     for (int i = 0; i < n  - 1; ++i) {
         for (int j = i + 1; j < n; ++j) {
-            Tile first = tiles[i];
-            Tile second = tiles[j];
+            Point first = tiles[i];
+            Point second = tiles[j];
             maxArea = max(maxArea, area(first, second));
-            at++;
         }
     }
-    cout << "At = " << at << endl;
     return maxArea;
+}
+
+ll part2(const vector<Point>& tiles) {
+    Polygon poly;
+    for (auto &p : tiles) {
+        bg::append(poly.outer(), p);
+    }
+    bg::correct(poly);
+    int n = tiles.size();
+    ll ans = - 1;
+    for (int i = 0; i < n - 1; ++i) {
+        for (int j = i + 1; j < n; ++j) {
+            ll x1 = min(tiles[i].x(), tiles[j].x());
+            ll x2 = max(tiles[i].x(), tiles[j].x());
+            ll y1 = min(tiles[i].y(), tiles[j].y());
+            ll y2 = max(tiles[i].y(), tiles[j].y());
+            ll area = (llabs(x1 - x2) + 1) * (llabs(y1 - y2) + 1);
+            Polygon rect;
+            bg::append(rect.outer(), Point(x1, y1));
+            bg::append(rect.outer(), Point(x1, y2));
+            bg::append(rect.outer(), Point(x2, y2));
+            bg::append(rect.outer(), Point(x2, y1));
+            bg::append(rect.outer(), Point(x1, y1));
+            bg::correct(rect);
+            if (area > ans && bg::within(rect, poly)) {
+                ans = area;
+            }
+        }
+    }
+    return ans;
 }
 
 int main() {
@@ -37,12 +69,12 @@ int main() {
         throw;
     }
     string str;
-    vector<Tile> tiles;
+    vector<Point> tiles;
     while (getline(fin, str)) {
         ll x,y;
         sscanf(str.c_str(), "%lld,%lld", &x, &y);
         tiles.push_back({x, y});
     }
-    cout << part1(tiles);
+    cout << part2(tiles);
     return 0;
 }
